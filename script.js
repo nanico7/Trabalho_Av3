88f5cb705c99de72da44b1d438f8d63e3d539dff
Thiago <neckelmachadothiago@gmail.com>
2025-10-02 19:03:24 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mudar cor do parágrafo
+document.getElementById("mensagem").style.color = "red";
+
+// Evento de clique no botão
+document.getElementById("botao").addEventListener("click", function() {
+    alert("Bem-vindo ao meu site!");
+});
